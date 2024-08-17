26b31c6e899fc8388f4c6e99625aebe3c6073a61
njuQRX <qrx@smail.nju.edu.cn>
2024-08-17 15:53:18 +0800

    add

---
+++ prepare-envs.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+set -x
+
+# Install dependencies
+apt-get update
+apt-get install -y build-essential python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools libgtk-3-dev
+apt-get install -y lld llvm llvm-dev clang
+apt-get install -y gcc-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-dev
+
+# Build and check whether the build has succeeded.
+make
+ls -lh afl-fuzz afl-cc
