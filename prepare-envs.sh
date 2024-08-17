c398288cd9474420b648e8b6876102637ab92fdc
njuQRX <qrx@smail.nju.edu.cn>
2024-08-17 16:02:09 +0800

    Remove sudo

--- prepare-envs.sh
+++ prepare-envs.sh
@@ -2,11 +2,12 @@
 
 set -x
 
+# Assume you are under root privilege.
 # Install dependencies
-sudo apt-get update
-sudo apt-get install -y build-essential python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools libgtk-3-dev
-sudo apt-get install -y lld llvm llvm-dev clang
-sudo apt-get install -y gcc-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-dev
+apt-get update
+apt-get install -y build-essential python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools libgtk-3-dev
+apt-get install -y lld llvm llvm-dev clang
+apt-get install -y gcc-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-dev
 
 # Build and check whether the build has succeeded.
 make
